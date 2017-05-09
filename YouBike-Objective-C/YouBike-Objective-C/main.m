372bb92f7d1676800157ef0232701bee8fc11254
CelesteTang <optimism2025@gmail.com>
2017-05-09 09:38:47 +0800

    Start YouBike-Objective-C

---
+++ YouBike-Objective-C/YouBike-Objective-C/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YouBike-Objective-C
+//
+//  Created by 湯芯瑜 on 2017/5/9.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

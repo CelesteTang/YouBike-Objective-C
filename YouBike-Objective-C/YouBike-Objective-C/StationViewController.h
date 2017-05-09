6c5614ff9ca9007450a8fe350c14be46b6db38df
CelesteTang <optimism2025@gmail.com>
2017-05-09 16:21:31 +0800

    [Add] set up tableViewCell

--- YouBike-Objective-C/YouBike-Objective-C/StationViewController.h
+++ YouBike-Objective-C/YouBike-Objective-C/StationViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface StationViewController : UIViewController
+@interface StationViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 
--- YouBike-Objective-C/YouBike-Objective-C/StationViewController.m
+++ YouBike-Objective-C/YouBike-Objective-C/StationViewController.m
@@ -17,11 +17,53 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [_listGridSegmentedControl addTarget:self action:@selector(indexChanged) forControlEvents:UIControlEventTouchUpInside];
+    self.title = @"YouBike";
+    
+    _tableView.hidden = false;
+    _collectionView.hidden = true;
+    
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    
+    [_listGridSegmentedControl addTarget:self action:@selector(indexChanged:) forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (void)indexChanged {
+- (void)indexChanged: (UISegmentedControl *) sender {
     
+    switch (sender.selectedSegmentIndex) {
+            
+        case 0:
+            _tableView.hidden = true;
+            _collectionView.hidden = false;
+            
+        case 1:
+            _tableView.hidden = false;
+            _collectionView.hidden = true;
+            
+        default:
+            break;
+    }
+    
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"StationTableViewCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
 }
 
 @end

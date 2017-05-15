926be5a561f84319d614694a33366c69095f0093
CelesteTang <optimism2025@gmail.com>
2017-05-15 15:27:00 +0800

    [Add] map and comment table view cell

---
+++ YouBike-Objective-C/YouBike-Objective-C/MapTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MapTableViewCell.m
+//  YouBike-Objective-C
+//
+//  Created by 湯芯瑜 on 2017/5/15.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+#import "MapTableViewCell.h"
+
+@implementation MapTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
